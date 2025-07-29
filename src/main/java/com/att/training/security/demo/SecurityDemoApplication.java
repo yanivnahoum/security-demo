fd76841c711c09877b976c1ac0286f635d427f73
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-07-29 12:24:15 +0300

    Initial commit

---
+++ src/main/java/com/att/training/security/demo/SecurityDemoApplication.java
@@ -0,0 +1,11 @@
+package com.att.training.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/att/training/security/demo/SecurityDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.att.training.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+    @Test
+    void contextLoads() {
+        // TBD
+    }
+}
