a5bfa5d3b6c75a6dda46986f3d903c1d92f0d86f
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-03-12 13:33:37 +0200

    Add JdbcClient to UserDao for enhanced SQL query execution

--- src/main/java/com/att/training/security/demo/UserDao.java
+++ src/main/java/com/att/training/security/demo/UserDao.java
@@ -2,6 +2,7 @@ package com.att.training.security.demo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -17,10 +18,18 @@ public class UserDao {
             WHERE name = '%s'
             """;
     private final JdbcTemplate jdbcTemplate;
-
+    private final JdbcClient jdbcClient;
 
     public List<User> findByName(String name) {
+        // This SQL injection is identified correctly by Veracode
         return jdbcTemplate.query(FIND_BY_NAME_SQL.formatted(name), this::mapToUser);
+    }
+
+    public List<User> findByName2(String name) {
+        // This SQL injection is NOT picked up by Veracode
+        return jdbcClient.sql(FIND_BY_NAME_SQL.formatted(name))
+                .query(this::mapToUser)
+                .list();
     }
 
     private User mapToUser(ResultSet resultSet, int num) throws SQLException {
